729bb1bd63fd9a471b828478a27d0c4b80e0a0c1
Joseph Fischetti <fischettijw@gmail.com>
2020-02-12 15:40:00 -0500

    Test change

--- frmGraphics.cs
+++ frmGraphics.cs
@@ -39,23 +39,21 @@ namespace cSharp___GDI__Graphics_Tutorial
 
         private void frmGraphics_Paint(object sender, PaintEventArgs e)
         {
-            //Font myFont = new Font("Arial", 14, FontStyle.Bold);
-            //Pen myPen = new Pen(Brushes.Black, 5);
-
             e.Graphics.Clear(Color.Yellow);
             e.Graphics.FillRectangle(Brushes.Blue, 100, 100, 50, 75);
             e.Graphics.DrawRectangle(Pens.Black, 10, 10, 50, 75);
             e.Graphics.DrawRectangle(Pens.Red, new Rectangle(50, 50, 75, 100));
             e.Graphics.DrawImage(Properties.Resources.stick, 200, 200);
             e.Graphics.DrawLine(Pens.Cyan, 200, 100, 300, 200);
-            e.Graphics.DrawString("hello, my name is Joe", this.Font, Brushes.Green, 150, 150);
-            e.Graphics.DrawString("hello, my name is Joe", myFont, Brushes.Black, 150, 175);
+            e.Graphics.DrawString("hello, my name is Joe",
+                this.Font, Brushes.Green, 150, 150);
+            e.Graphics.DrawString("hello, my name is Joe",
+                myFont, Brushes.Black, 150, 175);
             e.Graphics.DrawPie(Pens.Red, 300, 300, 100, 100, 0, 360);
             e.Graphics.FillPie(Brushes.Green, 600, 300, 100, 100, 0, 360);
             e.Graphics.FillPie(Brushes.DarkMagenta, 450, 200, 100, 100, 90, 270);
             e.Graphics.DrawArc(Pens.DarkMagenta, 450, 50, 100, 100, 90, 270);
             e.Graphics.DrawArc(myPen, 650, 50, 100, 100, 90, 270);
-
             e.Graphics.DrawPolygon(myPen, polyPoints);
 
             //createPolyPoints();
@@ -65,7 +63,6 @@ namespace cSharp___GDI__Graphics_Tutorial
         private void timAppTimer_Tick(object sender, EventArgs e)
         {
             this.Refresh();
-
         }
 
     }
